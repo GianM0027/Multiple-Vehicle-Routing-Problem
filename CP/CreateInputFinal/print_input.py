5cb4274664588236adc3cbf71faa4bfd3daa1f35
Gian Mario Marongiu <gianmario0027@gmail.com>
2023-05-29 17:47:42 +0200

    Output creation

--- CP/CreateInputFinal/print_input.py
+++ CP/CreateInputFinal/print_input.py
@@ -1,11 +1,10 @@
+import sys
 import numpy as np
-import datetime
-
+np.set_printoptions(threshold=sys.maxsize)
 
 def main():
-
     # Instantiate variables from file
-    instances_path = "Instances_CP/sorted_test.dzn"   #inserire nome del file
+    instances_path = "Instances/inst20.dat"   #inserire nome del file
     data_file = open(instances_path)
     lines = []
     for line in data_file:
@@ -33,14 +32,27 @@ def main():
     dist_str = str(dist)
     dist_str = dist_str[0] + dist_str[1:-1].replace("[", "|").replace("]", "|") + dist_str[-1]
     dist_str = dist_str.replace("\n", "")
+    dist_str = dist_str[0] + dist_str[1:-1].replace("  ", " ") + dist_str[-1] #eliminazione doppi spazi
+    dist_str = dist_str[0] + dist_str[1:-1].replace("   ", " ") + dist_str[-1]  #eliminazione tripli spazi
+    dist_str = dist_str[0] + dist_str[1:-1].replace("| ", "|") + dist_str[-1]
     dist_str = dist_str[0] + dist_str[1:-1].replace("| |", "|") + dist_str[-1]
+    dist_str = dist_str[0] + dist_str[1:-1].replace("||", "|") + dist_str[-1]
     dist_str = dist_str[0] + dist_str[1:-1].replace(" ", ", ") + dist_str[-1]
+    dist_str = dist_str[0] + dist_str[1:-1].replace(", ,", ", ") + dist_str[-1]
+
 
     print("n_couriers = " + str(n_couriers) + ";")
     print("n_items = " + str(n_items) + ";")
     print("max_load = " + str(max_load) + ";")
     print("size_item = " + str(size_item) + ";")
     print("all_distances = " + str(dist_str) + ";")
+
+    with open("input_test.dzn", "w") as output_file:
+        output_file.write("n_couriers = " + str(n_couriers) + ";\n")
+        output_file.write("n_items = " + str(n_items) + ";\n")
+        output_file.write("max_load = " + str(max_load) + ";\n")
+        output_file.write("size_item = " + str(size_item) + ";\n")
+        output_file.write("all_distances = " + str(dist_str) + ";")
 
 
 # instance_number = input("Inserire numero Instance [1 -5]")
