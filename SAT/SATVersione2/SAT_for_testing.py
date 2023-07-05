f53b8ea66ad10123da3325a769e1e489fc5300d6
Mauro Dore <82566433+maurodore@users.noreply.github.com>
2023-07-05 19:56:41 +0200

    PUSH PER GIMMY

---
+++ SAT/SATVersione2/SAT_for_testing.py
@@ -0,0 +1,27 @@
+NAIVEPAIRWISE = "NaivePairwise"
+SEQUENTIAL = "Sequentil"
+BITWISE = "Bitwise"
+HEULE = "Heule"
+
+encodings = [NAIVEPAIRWISE, SEQUENTIAL, BITWISE, HEULE]
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+def main():
+
+    n_istances = 21
+    count = 1
+
+    output = {}
+
