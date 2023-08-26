b2214b5f919b79d235d5085d7e5bc20dcedec3d6
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2023-08-26 17:42:44 +0200

    ciao

--- SMT/newSMT.py
+++ SMT/newSMT.py
@@ -1,4 +1,5 @@
 # - - - - - - - - - - - - - - - - - - - - - IMPORTS - - - - - - - - - - - - - - - - - - - - - #
+from matplotlib import cm, pyplot as plt
 from z3 import *
 import numpy as np
 from math import log2
@@ -96,17 +97,31 @@ def createGraph(all_distances):
 
     return G
 
+def print_graph(G, n_couriers, tour_edges, x, model):
+    # Calculate the node colors
+    colormap = cm._colormaps.get_cmap("Set3")
+    node_colors = {}
+    for z in range(n_couriers):
+        for i, j in G.edges:
+            if model.evaluate(x[i][j][z]):
+                node_colors[i] = colormap(z)
+                node_colors[j] = colormap(z)
+    node_colors[0] = 'pink'
+    # Convert to list to maintain order for nx.draw
+    color_list = [node_colors[node] for node in G.nodes]
 
-def print_loads(print_routes, max_l, s_item):
+    nx.draw(G.edge_subgraph(tour_edges), with_labels=True, node_color=color_list)
+    plt.show()
+
+
+def print_loads(model, print_routes, max_l, loads, s_item):
+    s_item = s_item[1:]
     print("\n- - Print Loads - -")
     print("Size Items: ", s_item)
     for r in print_routes:
         print(f"{print_routes.index(r)} - Max Load: {max_l[print_routes.index(r)]}")
-        load = 0
         print(r)
-        for i in range(1, len(r)):
-            load += s_item[r[i]]
-        print(f"Total Load: {load}\n")
+        print(f"Total Load: {model.evaluate(loads[print_routes.index(r)])}\n")
 
 
 def main(instance_num=1, remaining_time=300, upper_bound=None):
@@ -117,8 +132,11 @@ def main(instance_num=1, remaining_time=300, upper_bound=None):
     # Defining a graph which contain all the possible paths
     G = createGraph(all_distances)
 
+    # decision variables
     x = [[[Bool(f"x_{i}_{j}_{k}") for k in range(n_couriers)] for j in range(n_items + 1)] for i in
          range(n_items + 1)]  # x[i][j][k] == True : route (i->j) is used by courier k | set of Archs
+
+    courier_loads = [Int(f"courier_loads_{k}") for k in range(n_couriers)]
 
     u = [Int(f"u_{j}") for j in G.nodes]
 
@@ -165,21 +183,24 @@ def main(instance_num=1, remaining_time=300, upper_bound=None):
     # For each vehicle, the total load over its route must be smaller than its max load size
     for k in range(n_couriers):
         #s.add(PbLe([(v[i][k], size_item[i+1]) for i in range(n_items)], max_load[k]))
-        s.add(PbLe([(x[i+1][j+1][k], size_item[i]) for i in range(n_items) for j in range(n_items)],
-                   max_load[k]))
+        s.add(courier_loads[k] == Sum([If(x[i][j][k], size_item[i],0) for i, j in G.edges]))
+        s.add(courier_loads[k] > 0)
+        s.add(courier_loads[k] <= max_load[k])
 
     # - - - - - - - - - - - - - - - - - NO SUBTOURS PROBLEM - - - - - - - - - - - - - - - - - - - - - - #
 
     #  Miller-Tucker-Zemlin formulation
+    """
     for k in range(n_couriers):
         for i in range(n_items):
             for j in range(n_items):
                 s.add(u[i] + If(x[i][j][k], 1, 0) <= u[j] + n_items * (1 - If(x[i][j][k], 1, 0)))
                 s.add(u[i] > 0)
+    """
 
-    # - - - - - - - MTZ BUSACCHI - - - - - - - #
+    # - - - - - - - MTZ BUSACHI - - - - - - - #
 
-    """s.add(u[0] == 1)
+    s.add(u[0] == 1)
 
     # all the other points must be visited after the depot
     for i in G.nodes:
@@ -190,30 +211,39 @@ def main(instance_num=1, remaining_time=300, upper_bound=None):
     for z in range(n_couriers):
         for i, j in G.edges:
             if i != 0 and j != 0 and i != j:  # excluding the depot
-                s.add(x[i][j][z] * u[j] >= x[i][j][z] * (u[i] + 1))"""
+                s.add(x[i][j][z] * u[j] >= x[i][j][z] * (u[i] + 1))
 
     # - - - - - - - - - - - - - - - - - SOLVING - - - - - - - - - - - - - - - - - - - - - - #
 
     total_distance = Sum(
-        [If(x[i][j][k], int(all_distances[i][j]), 0) for k in range(n_couriers) for i in range(n_items + 1) for j in
-         range(n_items + 1)])
+        [If(x[i][j][k], int(all_distances[i][j]), 0) for k in range(n_couriers) for i,j in G.edges])
 
     min_distance = Sum(
-        [If(x[i][j][0], int(all_distances[i][j]), 0) for i in range(n_items + 1) for j in range(n_items + 1)])
+        [If(x[i][j][0], int(all_distances[i][j]), 0) for i,j in G.edges])
 
     max_distance = Sum(
-        [If(x[i][j][0], int(all_distances[i][j]), 0) for i in range(n_items + 1) for j in range(n_items + 1)])
+        [If(x[i][j][0], int(all_distances[i][j]), 0) for i,j in G.edges])
 
     for k in range(n_couriers):
         temp = Sum(
-            [If(x[i][j][k], int(all_distances[i][j]), 0) for i in range(n_items + 1) for j in range(n_items + 1)])
+            [If(x[i][j][k], int(all_distances[i][j]), 0) for i,j in G.edges])
         min_distance = If(temp < min_distance, temp, min_distance)
         max_distance = If(temp > max_distance, temp, max_distance)
 
+    """  
+    # OBJECTIVE 1
     if upper_bound is None:
         s.add(objective == Sum(total_distance, (max_distance - min_distance)))
     else:
         s.add(objective == Sum(total_distance, (max_distance - min_distance)))
+        s.add(upper_bound > objective)
+    """
+
+    # OBJECTIVE 2
+    if upper_bound is None:
+        s.add(objective == max_distance)
+    else:
+        s.add(objective == max_distance)
         s.add(upper_bound > objective)
 
     if s.check() == sat:
@@ -232,18 +262,22 @@ def main(instance_num=1, remaining_time=300, upper_bound=None):
         print("Max Distance: ", model.evaluate(max_distance))
         print("Total Distance: ", model.evaluate(total_distance))
 
-        print_loads(routes, max_load, size_item)
+        print_loads(model, routes, max_load, courier_loads, size_item)
 
         new_objective = model.evaluate(objective)
+
+        print_graph(G, n_couriers, edges_list, x, model)
 
         return new_objective
     else:
         print("\nMERDA")
+        print(all_distances)
         return 0
-
 
 inst = 1
 temp = main(inst, 300)
 
 for _ in range(10):
     temp = main(inst, 300, temp)
+    if temp == 0:
+        break
